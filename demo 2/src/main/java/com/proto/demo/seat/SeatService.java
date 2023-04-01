bf3ea44796a27a540e71bd10d4867c2ba6ab404d
CollinHarris <CollinHarris@localhost>
2023-04-01 15:44:47 -0400

    added service annotation

--- demo 2/src/main/java/com/proto/demo/seat/SeatService.java
+++ demo 2/src/main/java/com/proto/demo/seat/SeatService.java
@@ -7,6 +7,23 @@ import org.springframework.stereotype.Service;
  *
  * @author CollinHarris
  */
+@Service
 public class SeatService {
     
+    @Autowired
+    private SeatRepository repo;
+
+    public List<Seat> getAllSeats(){
+        return repo.findAll(); 
+    }
+    
+    public Seat getSeat(long SeatId){
+        return repo.getReferenceById(SeatId);
+    }
+    public void deleteSeat(long SeatId){
+        repo.deleteById(SeatId);
+    }
+    public void saveSeat(Seat seat){
+        repo.save(seat);
+    }
 }
